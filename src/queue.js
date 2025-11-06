bd34fe6ba05bfdf51f5b31e215c78f4ce9811e35
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2025-11-06 17:20:21 +0300

    feat: add queue solution

--- src/queue.js
+++ src/queue.js
@@ -13,19 +13,32 @@ const { NotImplementedError } = require('../lib/errors');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-  getUnderlyingList() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor() {
+    this.head = null;
+    this.tail = null;
   }
 
-  enqueue(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  getUnderlyingList() {
+    return this.head;
+  }
+
+
+  enqueue(value) {
+    const newElement = new ListNode(value);
+
+    if (this.head === null) {
+      this.head = newElement;
+    } else {
+      this.tail.next = newElement;
+    }
+
+    this.tail = newElement;
   }
 
   dequeue() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    const topElement = this.head.value;
+    this.head = this.head.next;
+    return topElement;
   }
 }
 
